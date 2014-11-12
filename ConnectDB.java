48fb713431ebfeb9e77782d8900b3ee984248ad8
Ry12ob <ryanrobinson@comcast.net>
2014-11-12 00:07:49 -0500

    A sample code for trying to connect to the DB, need to update with the user name and password for the DB once we get it.
    Dont know which is better between the Class. part or the register diver, need to test it.

---
+++ ConnectDB.java
@@ -0,0 +1,32 @@
+package DM;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+
+public class ConnectDB{
+    
+    public static void main(String[] args){
+        String userName="";//oracle account user name goes here
+        String password="";//password goes here
+        String url="jdbc:oracle:thin:@olympia.unfcsd.unf.edu:1521:dworcl";
+        Connection conn;
+        try{
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            //Can use either one need to test which will work. The one below was the one we used in SE
+            //Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
+            conn=DriverManager.getConnection(url, userName, password);
+            System.out.println("Connected to DB");
+            conn.close();
+        }
+        catch (Exception ie) {
+            if (ie.getMessage().equals("IO Error: The Network Adapter could not establish the connection")) {
+                System.out.println("Error The network adapter could not establish the connection\n"
+                        + "Make sure you are connected to the unf campus internet either locally or through a VPN"
+                        + "\nSee the unf support page for info on VPNing into the campus internet");
+            } else {
+                System.out.println(ie.getMessage());
+            }
+        }
+    }
+}
