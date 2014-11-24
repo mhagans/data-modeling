fa50f91e9295425dccb8734e177ebbb1e5899d68
Clayton Langston <claytonlangston@gmail.com>
2014-11-24 10:05:45 -0500

    Implemented linkedlist for storing and handling student objects

--- StudentMenu.java
+++ StudentMenu.java
@@ -1,13 +1,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 //Student menu; gets list of students, displays students allows a student to be selected, allows a student to be created
+//try to only run this once
 public class StudentMenu {
 	private String userName="teama1dm2f14";//oracle account user name goes here
     private String password="team1bcchlrt";//password goes here
     private String url="jdbc:oracle:thin:@olympia.unfcsd.unf.edu:1521:dworcl";
     private Connection conn;
+    private ArrayList Students;
     
     private void openDBcon(){	 
     	try{
@@ -39,13 +44,119 @@ public class StudentMenu {
 		}
     }
     
+    //Constructor builds and populates linked list of student objects
     StudentMenu(){
+    	Statement stmt = null;
+    	String query = "SELECT name FROM id WHERE Permission = 0";
     	
+    	LinkIterator iterator = new LinkIterator();
+    	
+    	try{
+    		openDBcon();						//Open the DB connection
+    		stmt = conn.createStatement();	
+    		 ResultSet rs = stmt.executeQuery(query);   		 
+    		 while (rs.next()) {
+    			 Student student = new Student();
+    			 student.setName(rs.getString("name"));
+    			 Link link = new Link();
+    			 link.setStudent(student);
+    			 
+    			 if(iterator.noFirstLink()){
+    				 iterator.setFirstLink(link);
+    			 }
+    			 
+    			 else{
+    				 iterator.getcurrentlink().setnextlink(link);
+    				 iterator.nextlink();
+    			 }    			 
+    		 }
+    		 closeDBcon();
+    	}
+    	
+    	catch (SQLException e ) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) { try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+          }
+        }
     }
     
     public void displayStudentMenu(){
-    	
-    }
-    
-    
+    	    	
+    }    
+}
+
+class Link{
+	  
+	  private Link nextlink;
+	  private Link prevlink;
+	  private Student student;
+	  
+	  
+
+	  public Student getStudent(){
+	    return(student);
+	  }
+
+	  public void setStudent(Student x){
+	    student = x;
+	  }
+
+	  public Link getnextlink(){
+	    return(nextlink);
+	  }
+
+	  public void setnextlink(Link x){
+	    nextlink = x;
+	  }
+
+	  public Link getprevlink(){
+	    return(prevlink);
+	  }
+
+	  public void setprevlink(Link x){
+	    prevlink = x;
+	  }
+}
+
+class LinkIterator{
+	  private Link currentlink;
+	  private Link firstlink;
+	  
+	  public boolean noFirstLink(){
+		  if(firstlink == null){
+			  return true;
+		  }
+		  else{
+			  return false;
+		  } 		  
+	  }
+	  
+	  public Link getFirstLink(){
+		  return firstlink;
+	  }
+	  
+	  public void setFirstLink(Link x){
+		  firstlink = x;
+	  }
+	  
+	  public void setcurrentlink(Link x){
+	    currentlink = x;
+	  }
+	  
+	  public Link getcurrentlink(){
+	    return(currentlink);
+	  }
+
+	  public void nextlink(){
+	   if(currentlink.getnextlink() != null)
+	     currentlink = currentlink.getnextlink();
+	   else
+	     System.out.print("End of list");   
+	}
 }
